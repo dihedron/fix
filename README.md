718d121f01cd58c636db35a4c2833021ac082b2e
Andrea Funtò <dihedron.dev@gmail.com>
2021-01-01 20:58:00 +0100

    Initil checkin

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# fix
-A utility to fix ripped MP3's names
+# fix - a utility to fix ripped MP3's names.
+
+## Install prerequisites for ripping CDs to MP3s
+
+In order to rip your CDs, install `abcde`, `cdparanoia` and `lame`, then add the following to your `.bashrc`:
+
+```bash
+alias rip="abcde -a cddb,read,encode,tag,clean,move -B -G -p -g -j 4 -o mp3:\"--preset extreme\" -x"
+```
+
+I have not checked whether it also requires packages `id3tools` and `id3v2`.
+
+## Compile `fix`
+
+In order to compile `rip`, you need a Golang compiler; simply clone the repo and run `go build`, then copy `fix` to your path.
+
+## Rip CDs and fix their names
+
+In a directory of your choice, run `rip`; it will calculate the dicid and use it to dowload CD info and artwork, prompting for confirmation.
+
+Once the rip process has completed, run `fix <name of directory>` and it will automatically adjust filenames.
+
+
+
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/jessevdk/go-flags"
+)
+
+// Options contains the application options.
+type Options struct {
+	Simulate bool `short:"s" long:"simulate" description:"Run in simulation mode (no renaming)."`
+	NoDirs   bool `short:"d" long:"skip-dirs" description:"Skip directory renaming."`
+	NoFiles  bool `short:"f" long:"skip-files" description:"Skip MP3 file renaming."`
+}
+
+type rename struct {
+	from string
+	to   string
+}
+
+func main() {
+
+	opts := Options{}
+	dirs, _ := flags.Parse(&opts)
+	if len(dirs) == 0 {
+		dirs = append(dirs, ".")
+	}
+
+	renames := []rename{}
+
+	if !opts.NoDirs {
+		for _, dir := range dirs {
+			err := filepath.Walk(dir,
+				func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					if info.IsDir() && info.Name() != "." && info.Name() != ".." && info.Name() != "albumart_backup" && strings.Contains(info.Name(), "_") {
+						re := regexp.MustCompile(`^([^-]+\S)(?:-)(\S[^-]+)$`)
+						groups := re.FindAllStringSubmatch(info.Name(), -1)
+						if len(groups) > 0 {
+							name := fmt.Sprintf("%s - %s", strings.ReplaceAll(groups[0][1], "_", " "), strings.ReplaceAll(groups[0][2], "_", " "))
+							newPath := strings.Replace(path, info.Name(), name, 1)
+							renames = append(renames, rename{
+								from: path,
+								to:   newPath,
+							})
+						}
+					}
+					return nil
+				})
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
+	if !opts.NoFiles {
+		for _, dir := range dirs {
+			err := filepath.Walk(dir,
+				func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					if info.Name() != "." && info.Name() != ".." {
+						re := regexp.MustCompile(`^((\d{0,2})\.)(.*)\.(mp3)$`)
+						groups := re.FindAllStringSubmatch(info.Name(), -1)
+						if len(groups) > 0 {
+							name := fmt.Sprintf("%s - %s.%s", groups[0][2], strings.ReplaceAll(groups[0][3], "_", " "), groups[0][4])
+							name = strings.ReplaceAll(name, "  ", " - ")
+							newPath := strings.Replace(path, info.Name(), name, 1)
+							renames = append(renames, rename{from: path, to: newPath})
+						}
+					}
+					return nil
+				})
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
+	for i := len(renames) - 1; i >= 0; i-- {
+		fmt.Printf("renaming %q to %q...", renames[i].from, renames[i].to)
+		if !opts.Simulate {
+			if err := os.Rename(renames[i].from, renames[i].to); err != nil {
+				fmt.Println(" KO!")
+			} else {
+				fmt.Println(" OK!")
+			}
+		} else {
+			fmt.Println(" SKIPPED!")
+		}
+	}
+}
